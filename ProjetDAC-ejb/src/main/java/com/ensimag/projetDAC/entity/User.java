d62987e01490668aef3ab63245df6e8d9240d682
John-Elie Margot <john-elie.margot@ensimag.grenoble-inp.fr>
2016-02-19 15:23:59 +0100

    Ajout utilisateur

---
+++ ProjetDAC-ejb/src/main/java/com/ensimag/projetDAC/entity/User.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ensimag.projetDAC.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author margotj
+ */
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String name;
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String firstName;
+
+    /**
+     * Get the value of firstName
+     *
+     * @return the value of firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @param firstName new value of firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String email;
+
+    /**
+     * Get the value of email
+     *
+     * @return the value of email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param email new value of email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String password;
+
+    /**
+     * Get the value of password
+     *
+     * @return the value of password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @param password new value of password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private boolean isManager = false;
+
+    /**
+     * Get the value of isManager
+     *
+     * @return the value of isManager
+     */
+    public boolean isIsManager() {
+        return isManager;
+    }
+
+    /**
+     * Set the value of isManager
+     *
+     * @param isManager new value of isManager
+     */
+    public void setIsManager(boolean isManager) {
+        this.isManager = isManager;
+    }
+
+    public User() {
+    }
+
+    public User(String name, String firstName, String email, String password) {
+        this.name = name;
+        this.firstName = firstName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String name, String firstName, String email, String password, boolean isManager) {
+        this.name = name;
+        this.firstName = firstName;
+        this.email = email;
+        this.password = password;
+        this.isManager = isManager;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.ensimag.projetDAC.entity.User[ id=" + id + " ]";
+    }
+    
+}
